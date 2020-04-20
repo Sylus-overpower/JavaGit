005b7b30a317dbe91c82db4d16c23b55e23a221b
305 <305@305-PC>
2020-04-20 12:02:19 +0900

    newthang

--- 20200420/src/EX05.java
+++ 20200420/src/EX05.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 
 public class EX05 {
 	public static void main(String[] args) {
 		System.out.println("스위치 공부하라고");
+		Scanner scan = new Scanner(System.in);
+		
+		int input = scan.nextInt();
+		//ctrl shift f 자동정렬
+		switch (input) {
+		case 1:
+			System.out.println("1입력했누?");
+			break;
+		case 2:
+			System.out.println("2입력했누?");
+			break;
+		case 3:
+			System.out.println("3? 돌았?");
+			break;
+
+		default:
+			System.out.println("있는거만 적어라 좀");
+			break;
+		}
 	}
 }
